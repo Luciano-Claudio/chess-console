79cbf9887fa30066af34aa24cb1ca50d1b360fdd
Luciano Claudio <lucianoclaudio.se@gmail.com>
2021-11-02 06:47:01 -0300

    Comecando a implementar a tela

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace xadrez_console.tabuleiro
+﻿namespace tabuleiro
 {
     class Tabuleiro
     {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
+        public Peca Peca(int linha, int coluna)
+        {
+            return Pecas[linha, coluna];
+        }
     }
 }
